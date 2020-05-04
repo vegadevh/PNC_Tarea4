8ae14f21ec58765244c2937b8af705c4f92aa612
vegadevh <00148816@uca.edu.sv>
2020-05-04 03:47:38 -0600

    Finalizado - Mensajes de errores y cambios en tipos de atributos

--- products/src/main/java/com/uca/capas/products/domain/Product.java
+++ products/src/main/java/com/uca/capas/products/domain/Product.java
@@ -1,28 +1,29 @@
 package com.uca.capas.products.domain;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Product {
 	
-	@NotNull
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Size(min=12, max=12, message="El id del producto debe tener exactamente 12 caracteres")
 	private String id;
-	@NotNull
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Size(min=1, max=100, message="El nombre del producto debe tener un maximo de 100 caractares")
 	private String name;
-	@NotNull
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Size(min=1, max=100, message="El nombre del producto debe tener un maximo de 100 caractares")
 	private String brand;
-	@NotNull
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Size(min=1, max=500, message="El nombre del producto debe tener 500 caractares")
 	private String desc;
-	@NotNull
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Digits(integer=10, fraction=0, message="No puede ingresar numeros decimales")
-	private Integer stock;
-	@NotNull
+	private String stock;
+	@NotEmpty(message="El campo no puede estar vacio")
 	@Pattern(regexp="^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$", message = "La fecha debe tener formato dia/mes/año")
 	private String dateIn;
 	
@@ -30,7 +31,7 @@ public class Product {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Product(String id, String name, String brand, String desc, Integer stock, @NotNull @Pattern(regexp = "^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$", message = "La fecha debe tener formato dia/mes/año") String dateIn) {
+	public Product(String id, String name, String brand, String desc, @NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") @NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") String stock, @NotNull @Pattern(regexp = "^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$", message = "La fecha debe tener formato dia/mes/año") String dateIn) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -72,11 +73,11 @@ public class Product {
 		this.desc = desc;
 	}
 
-	public Integer getStock() {
+	public @NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") @NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") String getStock() {
 		return stock;
 	}
 
-	public void setStock(Integer stock) {
+	public void setStock(@NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") @NotNull @Digits(integer = 10, fraction = 0, message = "No puede ingresar numeros decimales") String stock) {
 		this.stock = stock;
 	}
 
